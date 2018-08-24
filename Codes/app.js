244ebe66264d0f78707cc312dceb0049582c5b91
luk3Sky <ganindudananja@gmail.com>
2018-08-25 02:37:38 +0530

    Added the package.json file

--- Codes/app.js
+++ Codes/app.js
@@ -1,5 +1,14 @@
+console.log(__dirname);
+console.log(__filename);
+
+/*
 var time = 0;
-setInterval(){
+var timer = setInterval(function(){
     time += 2;
     console.log(time + " seconds have passed.");
-}
+
+    if (time >= 10){
+        clearInterval(timer);
+    }
+}, 2000);
+*/
